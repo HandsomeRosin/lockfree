fa5141c0374f45639c50adac241b3059798de49e
rosinyu(庾龙鑫) <rosinyu@tencent.com>
2019-07-30 10:15:51 +0800

    failed

--- List.go
+++ List.go
@@ -11,35 +11,28 @@ type ptr = unsafe.Pointer
 
 type List struct {
 	// NullVal 代表空值，在List为空时取值将会返回NullVal
-	nullVal		interface{}
-	head		itemPtr
-	tail		itemPtr
-	disabled	bool
-	elNum		int64
-	maxLength	int64
+	nullVal  interface{}
+	head     itemPtr
+	tail     itemPtr
+	disabled bool
+	elNum    int64
 }
 
 type item struct {
-	next		itemPtr
-	valPtr		ptr
+	next   itemPtr
+	valPtr ptr
 }
 
-// 创建一个无锁队列。defaultVal是取空链表时返回的默认值；maxSize是队列最大长度，若小于等于0，则默认其无限大。
-func CreateList(defaultVal interface{}, maxSize int64) List {
-	list := List {
-		nullVal: 	defaultVal,
-		disabled: 	false,
-		elNum: 		0,
-	}
-
-	if maxSize > 0 {
-		list.maxLength = maxSize
-	} else {
-		list.maxLength = INT64_MAX
+// 创建一个无锁队列。defaultVal是取空链表时返回的默认值；
+func CreateList(defaultVal interface{}) List {
+	list := List{
+		nullVal:  defaultVal,
+		disabled: false,
+		elNum:    0,
 	}
 
 	// head和tail都初始化为同一个空节点，该节点不存实际元素。
-	sentinel := itemPtr(&item{ nil, nil })
+	sentinel := itemPtr(&item{nil, nil})
 	list.head = sentinel
 	list.tail = sentinel
 
@@ -51,13 +44,10 @@ func (this *List) PushBack(val interface{}) (bool, bool) {
 	if this.disabled {
 		return false, false
 	}
-	if this.elNum >= this.maxLength {
-		return false, true
-	}
 
 	// 将新Val构造为item节点
 	node := &item{
-		next: nil,
+		next:   nil,
 		valPtr: ptr(&val),
 	}
 	node.valPtr = ptr(&val)
@@ -114,23 +104,12 @@ func (this *List) IsDisabled() bool {
 	return this.disabled == true
 }
 
-// 返回当前链表的长度
-func (this *List) Size() int64 {
-	return this.elNum
-}
-
 // 返回当前链表是否为空
 func (this *List) IsEmpty() bool {
 	return (*item)(this.head).next == nil
 }
 
-// 设置链表最大长度
-func (this *List) SetMaxSize(maxSize int64) {
-	this.maxLength = maxSize
-}
-
 func (this *List) Print() {
-	fmt.Print("size: ", this.elNum, ", data: ")
 	p := (*item)(this.head)
 	for p.next != nil {
 		fmt.Print(*((*interface{})((*item)(p.next).valPtr)), ",")
--- List_test.go
+++ List_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"sync"
 	"testing"
+	"time"
 )
 
 var defaultVal int
@@ -11,16 +12,17 @@ var queue List
 
 func init() {
 	defaultVal = -10086
-	queue = CreateList(defaultVal, 20)
+	queue = CreateList(defaultVal)
 }
 
-func TestBasic(t *testing.T) {
-	for i := 1; i <= 50; i++ {
+func TestBasicList(t *testing.T) {
+	t1 := time.Now()
+	for i := 1; i <= 1000000; i++ {
 		suc, _ := queue.PushBack(i)
-		if (!suc) {
-			fmt.Println("push fail: ", i)
+		if !suc {
+			// fmt.Println("push fail: ", i)
 		} else {
-			queue.Print()
+			// queue.Print()
 		}
 	}
 	queue.Disable()
@@ -31,42 +33,69 @@ func TestBasic(t *testing.T) {
 			break
 		}
 		if val == 0 {
-			fmt.Println("pop fail")
+			// fmt.Println("pop fail")
 		} else {
-			fmt.Print(val, " : ")
-			queue.Print()
+			// fmt.Print(val, " : ")
+			// queue.Print()
 		}
 	}
+	fmt.Println("用时：", time.Since(t1))
 }
-
 func TestList(t *testing.T) {
 	wgr := sync.WaitGroup{}
 	wgw := sync.WaitGroup{}
-	for i := 0; i < 4; i++ {
+	t1 := time.Now()
+	for i := 0; i < 1; i++ {
 		wgr.Add(1)
 		go reader(i*1000000, &wgr)
 	}
-	for i := 0; i < 4; i++ {
+	for i := 0; i < 1; i++ {
 		wgw.Add(1)
 		go writter(&wgw)
 	}
 	wgr.Wait()
 	queue.Disable()
 	wgw.Wait()
-	fmt.Println("total pop num: ", len(m))
+	fmt.Println("用时：", time.Since(t1))
+	// fmt.Println("total pop num: ", len(m))
 	fmt.Println("END-------------------------------------------")
 }
 
+func TestChannel(t *testing.T) {
+	wgr := sync.WaitGroup{}
+	wgw := sync.WaitGroup{}
+	t1 := time.Now()
+	for i := 0; i < 1; i++ {
+		wgr.Add(1)
+		go chReader(i*1000000, &wgr)
+	}
+	for i := 0; i < 1; i++ {
+		wgw.Add(1)
+		go chWriter(&wgw)
+	}
+	wgr.Wait()
+	close(ch)
+	wgw.Wait()
+	fmt.Println("用时：", time.Since(t1))
+	fmt.Println("END-------------------------------------------")
+}
+
+var dataNum int = 1000000
+var buffer int = 0
+var ch chan int = make(chan int, buffer)
+
+// var ch chan int = make(chan int, buffer)
+
 func reader(startNum int, wg *sync.WaitGroup) {
-	for i := 0; i < 50; i++ {
+	for i := 0; i < dataNum; i++ {
 		suc, enable := queue.PushBack(startNum + i)
-		for  !suc {
+		for !suc {
 			if !enable {
 				goto END
 			}
 			suc, enable = queue.PushBack(startNum + i)
 		}
-		// fmt.Println("push: ", startNum + i)
+		// fmt.Println("push: ", queue.Size())
 	}
 END:
 	wg.Done()
@@ -84,10 +113,28 @@ func writter(wg *sync.WaitGroup) {
 		if r == defaultVal {
 			continue
 		}
+		// fmt.Println("pop: ", r, "   ", queue.Size())
+		// mutex.Lock()
+		// m[r.(int)] = "" // 为了核对pop出来的数据总数是否与push进去的一样，为了防止竞争错误导致的重复，这里用map来防重
+		// mutex.Unlock()
+	}
+	wg.Done()
+}
+
+func chReader(startNum int, wg *sync.WaitGroup) {
+	for i := 0; i < dataNum; i++ {
+		ch <- i + startNum
+	}
+	wg.Done()
+}
+
+func chWriter(wg *sync.WaitGroup) {
+	for {
+		_, ok := <-ch
+		if !ok {
+			break
+		}
 		// fmt.Println("pop: ", r)
-		mutex.Lock()
-		m[r.(int)] = "" // 为了核对pop出来的数据总数是否与push进去的一样，为了防止竞争错误导致的重复，这里用map来防重
-		mutex.Unlock()
 	}
 	wg.Done()
 }
